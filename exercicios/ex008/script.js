09429f336271a987b6c1c5999685e2f4961b8672
GiovanneMika <mika.2023@alunos.utfpr.edu.br>
2025-08-16 18:47:54 -0300

    FinalÃização do exercicio 7

---
+++ exercicios/ex008/script.js
@@ -0,0 +1,7 @@
+//console.log('O console esta consolando!');
+var vel = 12;
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if (vel > 60) {
+    console.log (`Você ultrapassou a velocidade permitida! MULTADO!`);
+}
+console.log(`Dirija sempre utilizando cinto de segurança!`);
